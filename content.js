43ac1e332f0eed0b0dc1450683f5db3b9e0bd05c
Wang Cheng <wcyz666@126.com>
2015-05-16 14:40:31 +0800

    First commit

---
+++ content.js
@@ -0,0 +1,114 @@
+/**
+ * Created by lenovo on 2015/4/6.
+ */
+(function init(){
+
+    var MyUtils = (function (){
+
+        var ExtentionID = "gldpkdbfaapjnjlbajhhnjgpkamnllcn";
+
+        return {
+            ExtId : ExtentionID,
+            el : function(id, rg){
+                var range = rg || document;
+                return range.getElementById(id);
+            },
+            getMsg: function (what) {
+                return messages[what];
+            },
+            qs : function(selector, rg){
+                var range = rg || document;
+                return range.querySelector(selector);
+            },
+            qsa : function(selector, rg){
+                var range = rg || document;
+                return range.querySelectorAll(selector);
+            }
+        };
+    })();
+
+    var allowedType = ["email", "password", "text"];
+
+    window.onload = function(){
+
+        var pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
+
+        chrome.runtime.sendMessage(MyUtils.ExtId, "login_info", {}, function(response){
+            var regex,
+                is_auto;
+            if (pageHTML.indexOf("alert-success") != -1){
+                return;
+            }
+            var suc = fillForm(response);
+            if (pageHTML.indexOf("alert-danger") != -1){
+                regex = /<div class="alert alert-danger" role="alert">(.*?)<\/div>/;
+                alert(regex.exec(pageHTML)[1]);
+                return;
+            }
+            console.log(response);
+            is_auto = (response.is_auto === "true");
+            if (!suc) {
+                alert("Parameter Error");
+                return;
+            }
+            if (suc && is_auto){
+                MyUtils.el(response.form_id).submit();
+            }
+        });
+
+        chrome.runtime.onMessage.addListener(function (messages, sender, sendResponse) {
+            if (messages == "login_info") return false;
+            if (fillForm(messages))
+                sendResponse({status: "success"});
+            else
+                sendResponse({error: "Invalid value."});
+        });
+
+    };
+
+    function fillForm(messages){
+        var key,
+            item,
+            message = {};
+        //console.log(messages);
+        try{
+            message[messages.form_id] = "form_id";
+            message[messages.login_id] = messages["login_value"];
+            message[messages.password_id] = messages["password_value"];
+            message[messages.captcha_value] = getOCR(messages["captcha_pic_id"]);
+        }catch (ex){
+            return false;
+        }
+
+        //console.log(message);
+        for (key in message){
+            if (message[key].length < 1){
+                return false;
+            }
+            item = MyUtils.el(key);
+            //console.log(item);
+            if (item && item.tagName.toLowerCase() == "form")
+                continue;
+            if (item && item.tagName.toLowerCase() == "input" && allowedType.indexOf(item.type.toLowerCase()) != -1){
+                item.value = message[key];
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function getOCR(captcha_id) {
+        var image = new Image();
+        image.src = document.getElementById(captcha_id).src;
+        var canvas = document.createElement('canvas');
+        canvas.height = image.height;
+        canvas.width = image.width;
+        var imgDraw = canvas.getContext('2d');
+        imgDraw.drawImage(image,0,0);
+        return OCRAD(imgDraw);
+
+    }
+
+})();
---
+++ popup.js
@@ -0,0 +1,109 @@
+/**
+ * Created by lenovo on 2015/4/25.
+ */
+(function init(){
+    var MyUtils = (function (){
+
+        var keys = ["form_id", "login_id", "login_value", "password_id", "password_value", "captcha_pic_id", "captcha_value"];
+        var messages = {
+            saveSuccessMsg : "<div class='alert alert-success alert-dismissible' role='alert'><strong>Well done!</strong> You have saved all input.</div>",
+            saveFillSuccessMsg : "<div class='alert alert-success alert-dismissible' role='alert'><strong>Well done!</strong> You have saved and filled all input.</div>",
+            warningMsg : "<div class='alert alert-warning alert-dismissible' role='alert'><strong>Warning!</strong> Better check yourself, you're not looking too good.</div>"
+        };
+        var ExtentionID = "gldpkdbfaapjnjlbajhhnjgpkamnllcn";
+        var saveItem = function(name, value){
+            localStorage.setItem(name, value);
+        };
+
+        var getItem = function(name){
+            return localStorage.getItem(name) || "";
+        };
+
+        return {
+            ExtId : ExtentionID,
+            getKeys: function() {
+                return keys.slice(0);
+            },
+            getAll: function(){
+                var params = {},
+                    item;
+
+                for (var i = 0; i < keys.length; i++){
+                    item = getItem(keys[i]);
+                    if (item)
+                        params[keys[i]] = item;
+                }
+                params["is_auto"] = getItem("is_auto");
+                return params;
+            },
+            saveAll: function(params){
+                for (var key in params){
+                    if (params.hasOwnProperty(key))
+                        saveItem(key, params[key]);
+                }
+            },
+            el : function(id, rg){
+                var range = rg || document;
+                return range.getElementById(id);
+            },
+            getMsg: function (what) {
+                return messages[what];
+            },
+            qs : function(selector, rg){
+                var range = rg || document;
+                return range.querySelector(selector);
+            },
+            qsa : function(selector, rg){
+                var range = rg || document;
+                return range.querySelectorAll(selector);
+            }
+        };
+    })();
+    window.addEventListener("load", function(){
+
+        var form = document.forms[0],
+            key,
+            savedParams = MyUtils.getAll(),
+            params;
+        //console.log(savedParams);
+        for (key in savedParams){
+            if (savedParams.hasOwnProperty(key)) {
+                if (key == "is_auto")
+                    form.elements[key].checked = (savedParams[key] === "true");
+                else
+                    form.elements[key].value = savedParams[key];
+            }
+        }
+
+        var saveEvent = function(callback){
+
+            return function(event) {
+                event.preventDefault();
+
+                var keys = MyUtils.getKeys(),
+                    i;
+                params = {};
+                for (i = 0; i < keys.length; i++){
+                    params[keys[i]] = form.elements[keys[i]].value;
+                }
+                params["is_auto"] = form.elements["is_auto"].checked;
+                //console.log(params);
+                MyUtils.saveAll(params);
+                callback && callback.apply(null);
+            }
+        };
+        MyUtils.el("save").onclick = saveEvent(function(){
+            MyUtils.el("messages").innerHTML = MyUtils.getMsg("saveSuccessMsg");
+        });
+        MyUtils.el("fill-save").onclick = saveEvent(function(){
+            chrome.tabs.query({active:	true, currentWindow: true},
+                function(tabs)	{
+                    chrome.tabs.sendMessage(tabs[0].id, params, function(response){
+                        MyUtils.el("messages").innerHTML = ((response && response.error) ? MyUtils.getMsg("warningMsg") : MyUtils.getMsg("saveFillSuccessMsg"));
+                    });
+                });
+        });
+
+    }, false);
+
+})();
