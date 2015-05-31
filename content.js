856c93e36eb14a8de6d70a11297f20b2b013d46f
Wang Cheng <wcyz666@126.com>
2015-05-31 18:47:31 +0800

    Version 0.0.1
    	1. Add support for Autolab
    	2. Refactor current code

--- content.js
+++ content.js
@@ -25,24 +25,23 @@
     })();
 
 
-    document.onload = function(){
 
-        var pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
+    var url = window.location.href,
+        pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
 
-        if (window.location.href == "https://login.cmu.edu/idp/Authn/Stateless") {
-            chrome.runtime.sendMessage(MyUtils.ExtId, "login_info", {}, function (response) {
-                var is_auto;
+    if (url.indexOf("https://autolab.cs.cmu.edu/auth/users/sign_in") != -1){
+        window.location.href = "https://autolab.cs.cmu.edu/auth/users/auth/shibboleth";
+    }
+    else if (url.indexOf("https://login.cmu.edu/idp/Authn/Stateless") != -1) {
+        chrome.runtime.sendMessage(MyUtils.ExtId, "login_info", {}, function (response) {
+            var is_auto;
 
-                MyUtils.el("j_username").value = response["login_value"];
-                MyUtils.el("j_password").value = response["password_value"];
-                is_auto = (new Boolean(response.is_auto) == true);
-                if (is_auto && pageHTML.indexOf("Authentication failed.") == -1) {
-                    document.forms["query"]["submit"].click();
-                }
-            });
-        }
-
-    };
-
-
+            MyUtils.el("j_username").value = response["login_value"];
+            MyUtils.el("j_password").value = response["password_value"];
+            is_auto = (new Boolean(response.is_auto) == true);
+            if (is_auto && pageHTML.indexOf("Authentication failed.") == -1) {
+                document.forms["query"]["submit"].click();
+            }
+        });
+    }
 })();
