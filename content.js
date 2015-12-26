87d915d0fae91966c3a65ff397f360bfb128386a
WANG Cheng <wcyz666@126.com>
2015-12-25 23:20:09 -0800

    Version 0.0.3
        Fixed a bug in login
        Add support for Tartantrak

--- content.js
+++ content.js
@@ -17,9 +17,11 @@
     })();
 
 
-
     var url = window.location.href,
         pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
+
+
+
     if (url.indexOf("https://autolab.cs.cmu.edu/auth/users/sign_in") != -1){
         window.location.href = "https://autolab.cs.cmu.edu/auth/users/auth/shibboleth";
     }
@@ -29,15 +31,27 @@
     else if (url.indexOf("https://oli.cmu.edu/jcourse/webui/login/required.do?") != -1) {
         window.location.href = "https://oli.cmu.edu/jcourse/webui/login/webiso.do";
     }
-    else if (url.indexOf("https://login.cmu.edu/idp/Authn/Stateless") != -1) {
+    else if (url.indexOf("https://login.cmu.edu/idp") != -1) {
         chrome.runtime.sendMessage(MyUtils.ExtId, "login_info", {}, function (response) {
             var is_auto;
-
+            console.log(response);
             MyUtils.el("j_username").value = response["login_value"];
             MyUtils.el("j_password").value = response["password_value"];
             is_auto = (response.is_auto === "true");
             if (is_auto && pageHTML.indexOf("Authentication failed.") == -1) {
-                document.forms["query"]["submit"].click();
+                document.forms["query"]['_eventId_proceed'].click()
+            }
+        });
+    }
+    else if (url.indexOf("http://www.cmu.edu/career/") != -1) {
+        chrome.runtime.sendMessage(MyUtils.ExtId, "login_info", {}, function (response) {
+            var is_auto;
+
+            document.querySelector("input[alt='Username']").value = response["login_value"] + "@andrew.cmu.edu";
+            document.querySelector("input[alt='Password']").value = response["password_value"];
+            is_auto = (response.is_auto === "true");
+            if (is_auto && pageHTML.indexOf("Authentication failed.") == -1) {
+                document.forms[1].submit();
             }
         });
     }
